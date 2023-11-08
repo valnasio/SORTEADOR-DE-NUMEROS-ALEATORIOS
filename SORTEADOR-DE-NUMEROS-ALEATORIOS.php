b25c82aa11f5e69a0158de436b1dae5b69d04a24
Rafael ValnásIo <127369635+valnasio@users.noreply.github.com>
2023-11-07 21:18:26 -0300

    SORTEADOR-DE-NUMEROS-ALEATORIOS.php

--- SORTEADOR-DE-NUMEROS-ALEATORIOS.php
+++ SORTEADOR-DE-NUMEROS-ALEATORIOS.php
@@ -5,12 +5,24 @@
     <title>Formulário PHP</title>
     <style>
         body {
-            background: linear-gradient(to bottom, #FFA500, #FF8C00);
+            
+             background: rgb(0,0,0);
+             background: linear-gradient(306deg, rgba(0,0,0,1) 0%, rgba(22,1,110,1) 50%, rgba(0,0,0,1) 100%); 
             font-family: Arial, sans-serif;
             width: 100vw;
             height: 100vh;
             margin: 0;
             padding: 0;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+        }
+
+        h1 {
+            color:#fff;
+            font-family: 'Comic Sans MS', sans-serif;
+            
         }
 
         .container {
@@ -21,6 +33,16 @@
             padding: 20px;
             text-align: center;
             box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+
+        .form {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
         }
 
         label {
@@ -33,7 +55,7 @@
             width: 100%;
             padding: 10px;
             border: none;
-            background-color: #CDC1C5;
+            background-color: #F7F7F7;
             border-radius: 5px;
             margin-bottom: 10px;
         }
@@ -42,31 +64,52 @@
             width: 100%;
             padding: 10px;
             border: none;
-            background: #FFA500;
+            background: rgb(1, 8, 37);
+            color: white;
             border-radius: 5px;
-            color: #fff;
             font-weight: bold;
             cursor: pointer;
         }
 
         input[type="submit"]:hover {
-            background: #FF8C00;
+            background: rgb(1, 8, 37);
         }
 
         .result {
             font-weight: bold;
             margin-top: 10px;
+            animation: pulse 1s infinite; /* Animação de pulsação */
+        }
+
+        @keyframes pulse {
+            0% {
+                opacity: 1;
+            }
+            50% {
+                opacity: 0;
+            }
+            100% {
+                opacity: 1;
+            }
+        }
+
+        .image {
+            max-width: 150px; /* Máximo de metade da largura do container */
+            max-height: 150px; /* Máximo de metade da altura do container */
+            margin-bottom: 20px;
         }
     </style>
 </head>
 
 <body>
+    <h1>Drawing Uncertain Fates</h1>
+    <img src="https://i0.wp.com/imagensemoldes.com.br/wp-content/uploads/2020/05/Figura-Dado-PNG.png?fit=1074%2C1098&ssl=1" alt="Imagem" class="image">
     <div class="container">
-        <form method="post" action="SORTEADOR-DE-NUMEROS-ALEATORIOS.php">
+        <form class="form" method="post" action="index.php">
             <label for="valor1">Insira o valor inicial:</label>
             <input type="text" name="valor1" id="valor1" />
 
-            <label for="valor2">Insira o valor final:</label>
+            <label for "valor2">Insira o valor final:</label>
             <input type="text" name="valor2" id="valor2" />
 
             <input type="submit" value="Enviar">
